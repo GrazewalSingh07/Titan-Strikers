f0815eb22708148edcbe7e7a69a90267f28ceb55
GrazewalSingh <singhgrazewal2@gmail.com>
2022-07-27 21:31:28 +0530

    ./

---
+++ BackEnd/src/configs/db.js
@@ -0,0 +1,6 @@
+const mongoose=require("mongoose")
+ 
+
+module.exports=()=>{
+    return mongoose.connect("mongodb+srv://Grazewal:9199770959@cluster0.arpnr.mongodb.net/tittan-strikers")
+}
--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -4,8 +4,9 @@ const connect= require("./configs/db")
 app.listen(4000,async()=>{
     try {
         await connect()
+        console.log("Its Titan ka server")
 
     } catch (error) {
-        
+        console.log(error.message)
     }
 })
