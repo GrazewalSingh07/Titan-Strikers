e9ae62f8112ad15b9d18fcc6605154f1efd82831
Divya <99958902+Divya3419@users.noreply.github.com>
2022-07-29 09:21:31 +0530

    done

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <Container m="auto" maxW="container.2xl" backgroundSize="cover" backgroundRepeat="no-repeat" backgroundImage="https://images.unsplash.com/photo-1601807576163-587225545555?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80">
        
       <Container margin="auto" height="100vh"  width="100%">
-        <Heading  color="white">blah blahk blah blah balh</Heading>
+        
 
       </Container>
       
