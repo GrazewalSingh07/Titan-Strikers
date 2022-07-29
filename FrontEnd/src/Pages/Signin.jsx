bcb9595ad8537d7edec892b5febcec00405321af
GrazewalSingh <singhgrazewal2@gmail.com>
2022-07-29 09:20:50 +0530

    friday morning first commit

--- FrontEnd/src/Pages/Signin.jsx
+++ FrontEnd/src/Pages/Signin.jsx
@@ -1,4 +1,4 @@
-import { Container,Input ,Stack,Button,Heading} from "@chakra-ui/react"
+import { Container,Input ,Stack,Button,Heading,Image, HStack} from "@chakra-ui/react"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import {useDispatch} from "react-redux"
@@ -23,12 +23,19 @@ export const Signin=()=>{
             alert("please input email and password")
         }
     }
-    return <Container margin="auto"  maxW="container.sm" p={10}>
-        <Heading p={5}>Sign in</Heading>
-       <Stack spacing={5}>
-       <Input  onChange={(e)=>setdata({...data,email:e.target.value})}placeholder="email" value={data.email}/>
-        <Input  onChange={(e)=>setdata({...data,password:e.target.value})}placeholder="password" value={data.password}/>
-       </Stack>
-       <Button onClick={handlelogin}>Login</Button>
+    return <Container margin="auto"  maxW="container.xl" p={10}>
+        <HStack>
+            <Container>
+            <Heading p={5}>Sign in</Heading>
+                <Stack spacing={5}>
+                <Input  onChange={(e)=>setdata({...data,email:e.target.value})}placeholder="email" value={data.email}/>
+                    <Input  onChange={(e)=>setdata({...data,password:e.target.value})}placeholder="password" value={data.password}/>
+                </Stack>
+                <Button width="100%" colorScheme="green" mt={10} onClick={handlelogin}>Login</Button>
+                <Button width="100%" colorScheme="green" mt={10} onClick={()=>navigate("/signup")}>Don't have an account? Register Now</Button>
+            </Container>
+            <Image  p={50} width="50%" src="https://www.nicepng.com/png/detail/385-3851795_welcome-back-to-school-clipart-at-getdrawings-back.png"/>
+        </HStack>
+     
     </Container>
 }
